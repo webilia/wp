48aea0dc5fa0258d82e1f3fd3b41b4817a534fe2
hoseinrafiei <hoseinrafiei@gmail.com>
2022-10-13 21:50:46 -0700

    Added singletong abstract class and removed singleton interface

---
+++ src/Singleton.php
@@ -0,0 +1,42 @@
+<?php
+namespace Webilia\WP;
+
+use Webilia\WP\Helpers\Arr;
+
+/**
+ * Singleton Class
+ *
+ * @package Utils
+ */
+abstract class Singleton
+{
+    /**
+     * Constructor
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    final public static function getInstance()
+    {
+        static $instances = [];
+
+        $class = get_called_class();
+        if(!Arr::isset($instances, $class))
+        {
+            $instances[$class] = new $class();
+        }
+
+        return $instances[$class];
+    }
+
+    /**
+     * @return void
+     */
+    final private function __clone()
+    {
+    }
+}
