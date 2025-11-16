a2b04c5c151715a50ba216a6628e872f379fdd75
Hossein Rafiei <hoseinrafiei@gmail.com>
2025-11-15 22:24:02 -0500

    Add SiteReport class for posting reports

---
+++ src/SiteReport.php
@@ -0,0 +1,36 @@
+<?php
+namespace Webilia\WP;
+
+class SiteReport
+{
+    /**
+     * Send a report to Webilia.
+     *
+     * @param string $basename Required. Plugin basename.
+     * @param string $url Required. Site URL.
+     * @param array $report Required. Report data.
+     *
+     * @return bool
+     */
+    public static function send(string $basename, string $url, array $report): bool
+    {
+        $basename = sanitize_text_field($basename);
+        $url = esc_url_raw($url);
+
+        if ($basename === '' || $url === '' || empty($report)) return false;
+
+        $response = wp_remote_post('https://api.webilia.com/report', [
+            'body' => [
+                'basename' => $basename,
+                'url' => $url,
+                'report' => json_encode($report),
+            ],
+        ]);
+
+        if (is_wp_error($response)) return false;
+
+        $code = wp_remote_retrieve_response_code($response);
+
+        return $code === 200;
+    }
+}
